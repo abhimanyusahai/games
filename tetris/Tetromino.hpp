d03074d9d9a18f660ea2f3ad95d3fd569c7c54b1
Abhimanyu Sahai <sahai.abhimanyu@gmail.com>
2016-11-05 14:46:59 +0100

    Adding tetris v1 to github

---
+++ tetris/Tetromino.hpp
@@ -0,0 +1,43 @@
+//
+//  Tetromino.hpp
+//  tetris
+//
+//  Created by Abhimanyu Sahai on 05/11/16.
+//  Copyright © 2016 Abhimanyu Sahai. All rights reserved.
+//
+
+#ifndef Tetromino_hpp
+#define Tetromino_hpp
+
+#include "GridObjects.hpp"
+#include <random>
+#include "Resources.cpp"
+
+class Tetromino
+{
+public:
+    GridCellMatrix m_boundingBox;
+    
+private:
+    sf::Vector2i m_position; // Coordinates of top left cell of tetromino bounding box
+    GridCell::Type m_type;
+    std::vector<sf::Vector2i> m_clearingCellCoords; // Coordinates of cells occupied by the tetromino in its previous position, that need to be cleared by the grid
+    
+public:
+    Tetromino();
+    Tetromino(Tetromino &tetromino);
+    void generateRandom();
+    void initialize();
+    GridCell::Type getType();
+    friend Grid::GridMessage Grid::update(sf::RenderWindow &window, Tetromino &tetromino);
+    bool inRestingPosition(Grid &grid);
+    bool collidesOnLeft(Grid &grid);
+    bool collidesOnRight(Grid &grid);
+    void moveDownOneCell();
+    void shiftLeft(Grid &grid);
+    void shiftRight(Grid &grid);
+    void rotate(Grid &grid);
+    void draw(sf::RenderWindow &window);
+};
+
+#endif /* Tetromino_hpp */
